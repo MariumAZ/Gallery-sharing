a5b75edca6b533ddba528ce20b5fe8736edff700
MariumAZ <mariem.azzouz@dauphine.tn>
2021-12-17 17:38:40 +0100

    added not found page

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	// it is not obsvious that wwe are working always withh html
 	// so we set the content header
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site")
 }
+
+func contact(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+    fmt.Fprint(w, "To get in touch please send an email please contact <a href=\"mail to : myriam.azzouz@holbertonschool.com\">myriam.azzouz@holbertonschool.com<a/>.")
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Sorry Page Requested Not Found")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(NotFound)
+	r.HandleFunc("/", home)
+    r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
