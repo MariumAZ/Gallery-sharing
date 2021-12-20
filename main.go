e89d24cb6b69a3e4b162154b992ead389e422a94
MariumAZ <mariem.azzouz@dauphine.tn>
2021-12-20 12:37:26 +0100

    added contact and home views

--- main.go
+++ main.go
@@ -2,32 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 
 func home(w http.ResponseWriter, r *http.Request) {
 	// it is not obsvious that wwe are working always withh html
 	// so we set the content header
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
-func contact(w http.ResponseWriter, r *http.Request){
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-    fmt.Fprint(w, "To get in touch please send an email please contact <a href=\"mail to : myriam.azzouz@holbertonschool.com\">myriam.azzouz@holbertonschool.com<a/>.")
+    if err := contactTemplate.Execute(w, nil); err!= nil {
+		panic(err)
+	}
 }
 
-func NotFound(w http.ResponseWriter, r *http.Request){
+func NotFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "Sorry Page Requested Not Found")
 }
 
 func main() {
+	// keep track of the template being parsed
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(NotFound)
 	r.HandleFunc("/", home)
-    r.HandleFunc("/contact", contact)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
